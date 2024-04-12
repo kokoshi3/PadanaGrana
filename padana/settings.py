19317918eae7731cad0de4fd782711c998336e5e
Roksana <111154@g.elearn.uz.zgora.pl>
2024-04-12 19:09:04 +0200

    Change in settings.py for static files access

--- padana/settings.py
+++ padana/settings.py
@@ -119,9 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
