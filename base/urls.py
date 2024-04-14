27a415cea44090e9510d1c2606085a3715e03280
jakubbonczyk <jakubbonczyk@icloud.com>
2024-04-14 17:27:52 +0200

    Database

--- base/urls.py
+++ base/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('zamow/', views.zamow, name="zamow"),
-    path('login/', views.login, name="login"),
+    path('login/', views.login_view, name="login"),
     path('rejestracja/', views.rejestracja, name="rejestracja"),
     path('panelPracownika/', views.panelPracownika, name="panelPracownika"),
     path('panelKlienta/', views.panelKlienta, name="panelKlienta"),
--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.shortcuts import HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -12,8 +14,18 @@ def zamow(request):
 def rezerwacja(request):
     return render(request,'rezerwacja.html')
 
-def login(request):
-    return render(request,'login.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/panelPracownika')  # zmień '/home' na URL docelowy po zalogowaniu
+        else:
+            return HttpResponse("Nieprawidłowa nazwa użytkownika lub hasło.")
+    return render(request, 'login.html')  # Nazwa twojego pliku HTML dla formularza logowania
+    # return render(request,'login.html')
 
 def rejestracja(request):
     return render(request,'rejestracja.html')
