858323c105b07858b797d723ac093d853893828e
Roksana <111154@g.elearn.uz.zgora.pl>
2024-04-14 23:50:42 +0200

    Login for staff and admin

--- base/urls.py
+++ base/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     path('login/', views.login_view, name="login"),
     path('rejestracja/', views.rejestracja, name="rejestracja"),
     path('panelPracownika/', views.panelPracownika, name="panelPracownika"),
+    path('panelAdmina/', views.panelAdmina, name="panelAdmina"),
     path('panelKlienta/', views.panelKlienta, name="panelKlienta"),
     path('zamow/', views.zamow, name="zamow"),
     path('rezerwacja/', views.rezerwacja, name="rezerwacja"),
+
 
 ]
--- base/views.py
+++ base/views.py
@@ -1,42 +1,54 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.shortcuts import HttpResponse
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
-    return render(request,'homepage.html')
+    return render(request, 'homepage.html')
+
 
 def zamow(request):
-    return render(request,'zamow.html')
+    return render(request, 'zamow.html')
+
 
 def rezerwacja(request):
-    return render(request,'rezerwacja.html')
+    return render(request, 'rezerwacja.html')
+
 
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+        if user is not None and user.is_superuser:
             login(request, user)
-            return redirect('/panelPracownika')  # zmień '/home' na URL docelowy po zalogowaniu
+            return redirect('/panelAdmina')  # zmień '/home' na URL docelowy po zalogowaniu
+        elif user is not None and user.is_staff:
+            login(request, user)
+            return redirect('/panelPracownika')
         else:
             return HttpResponse("Nieprawidłowa nazwa użytkownika lub hasło.")
     return render(request, 'login.html')  # Nazwa twojego pliku HTML dla formularza logowania
     # return render(request,'login.html')
 
-def rejestracja(request):
-    return render(request,'rejestracja.html')
 
+def rejestracja(request):
+    return render(request, 'rejestracja.html')
+
+
+@login_required
 def panelPracownika(request):
-    return render(request,'panelPracownika.html')
+    return render(request, 'panelPracownika.html')
+
+
+@login_required
+def panelAdmina(request):
+    return render(request, 'panelAdmina.html')
+
 
 def panelKlienta(request):
-    return render(request,'panelKlienta.html')
-
-
-
-
-
+    return render(request, 'panelKlienta.html')
