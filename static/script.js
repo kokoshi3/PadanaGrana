828e2599b64c1be6b26576631449aaa95d61640e
jakubbonczyk <jakubbonczyk@icloud.com>
2024-04-15 16:44:06 +0200

    Added a booking app

--- static/script.js
+++ static/script.js
@@ -1,1 +1,8 @@
-
+  document.addEventListener('DOMContentLoaded', function () {
+      const myButton = document.getElementById('reservation');
+      myButton.onclick = function () {
+      console.log("klik");
+       Calendly.initPopupWidget({url: 'https://calendly.com/padanagrana-restaurant'});
+      return false;
+    };
+  })
