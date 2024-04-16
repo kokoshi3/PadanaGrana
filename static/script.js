8fb2fa474611b786bc2ac62b4d1add17d21721a1
jakubbonczyk <jakubbonczyk@icloud.com>
2024-04-16 03:05:41 +0200

    Added a structure of a employee panel

--- static/script.js
+++ static/script.js
@@ -1,8 +1,44 @@
-  document.addEventListener('DOMContentLoaded', function () {
-      const myButton = document.getElementById('reservation');
-      myButton.onclick = function () {
-      console.log("klik");
-       Calendly.initPopupWidget({url: 'https://calendly.com/padanagrana-restaurant'});
+document.addEventListener('DOMContentLoaded', function () {
+  let currentWindow = '';
+
+  const myButton = document.getElementById('reservation');
+  if (myButton) {
+    myButton.onclick = function () {
+      Calendly.initPopupWidget({url: 'https://calendly.com/padanagrana-restaurant'});
       return false;
     };
-  })
+  }
+
+  const buttons = document.querySelectorAll('.functional-buttons .func');
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      let windowName = button.getAttribute('data-window');
+      if (windowName !== currentWindow) {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+
+        currentWindow = windowName;
+        loadWindow(windowName);
+      }
+    });
+  });
+
+  function loadWindow(windowName) {
+    let templateId = windowName + '-template';
+    let template = document.getElementById(templateId);
+    if (template) {
+      let rightPanel = document.querySelector('.right-panel');
+      rightPanel.classList.remove('right-panel-content');
+      rightPanel.classList.add('fadeOut');
+
+      setTimeout(function () {
+        rightPanel.classList.remove('fadeOut');
+        rightPanel.innerHTML = '';
+        let clone = document.importNode(template.content, true);
+        rightPanel.appendChild(clone);
+        rightPanel.offsetWidth;
+        rightPanel.classList.add('right-panel-content');
+      }, 100);
+    }
+  }
+});
