06130415b70c68fe002844b6427132d085d38814
OlekZaremba <sarutern.gra@gmail.com>
2024-04-17 00:19:49 +0200

    ???

--- base/models.py
+++ base/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('otrzymany', 'Otrzymany'),
+        ('w_przygotowaniu', 'W_przygotowaniu'),
+        ('gotowy', 'Gotowy'),
+        ('zakonczony', 'Zakonczony')
+    ]
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='otrzymany')
+    total_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.status}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    quantity = models.IntegerField()
+    item_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
